8027729917c71d0b72a6ef37c216b570eb26a632
Ranjith.Chaz <Ranjith.Chaz@CanadaLife.de>
2024-03-03 19:47:47 +0000

    Add basic Map, JsonNode support

--- src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
@@ -1,5 +1,6 @@
 package guru.springframework.spring6resttemplate.client;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import guru.springframework.spring6resttemplate.model.BeerDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -8,18 +9,37 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Map;
+
 @RequiredArgsConstructor
 @Service
 public class BeerClientImpl implements BeerClient {
+    public static final String BASE_URL = "http://localhost:8080";
+    public static final String BEER_PATH = "/api/v1/beer";
+
     private final RestTemplateBuilder restTemplateBuilder;
     @Override
     public Page<BeerDTO> listBeers() {
         RestTemplate restTemplate = restTemplateBuilder.build();
         ResponseEntity<String> stringResponse = restTemplate.getForEntity(
-                "http://localhost:8080/api/v1/beer", String.class
+                BASE_URL + BEER_PATH, String.class
         );
 
-        System.out.println(stringResponse.getBody());
+        ResponseEntity<Map> mapResponse = restTemplate.getForEntity(
+                BASE_URL + BEER_PATH, Map.class
+        );
+
+        ResponseEntity<JsonNode> jsonResponse = restTemplate.getForEntity(
+                BASE_URL + BEER_PATH, JsonNode.class
+        );
+
+        jsonResponse.getBody().findPath("_embedded").findPath("beer")
+                .elements().forEachRemaining(
+                        jsonNode -> {
+                            System.out.println(jsonNode.get("beerName").asText());
+                        });
+
+        //System.out.println(stringResponse.getBody());
         return null;
 
 
